d76246514e7af0df0cd712c5cba5b50d3a2dd664
Andy Sarroff <sarroff@cs.dartmouth.edu>
2016-09-07 14:42:49 -0400

    tanh_grad and SimpleBinaryOp

---
+++ complex_tf/core/ops/cwise_cplx_ops_gradients.h
@@ -0,0 +1,16 @@
+#ifndef CTF_CWISE_CPLX_OPS_GRADIENTS_H_
+#define CTF_CWISE_CPLX_OPS_GRADIENTS_H_
+#include "tensorflow/core/kernels/cwise_ops_common.h"
+
+namespace tensorflow {
+  
+  namespace functor {
+    
+    struct CplxTanhGradKernelLauncher;
+    template <typename T>
+      struct cplx_tanh_grad : base<T, CplxTanhGradKernelLauncher> { };    
+
+  }  // namespace functor
+  
+}  // namespace tensorflow
+#endif  // CTF_CWISE_CPLX_OPS_GRADIENTS_H_
