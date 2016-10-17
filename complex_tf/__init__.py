b43ce29e6689490caa3f3e9fed3c4f0d03b24c16
Andy Sarroff <sarroff@cs.dartmouth.edu>
2016-10-17 12:15:00 -0400

    add suport for basic complex rnn

--- complex_tf/__init__.py
+++ complex_tf/__init__.py
@@ -5,3 +5,4 @@ path = (os.path.dirname(os.path.realpath(__file__)) +
         '/core/kernels/complextf.so')
 tf.load_op_library(path)
 import python.training as train
+import python.ops.nn as nn
---
+++ complex_tf/python/__init__.py
@@ -0,0 +1,1 @@
+from complex_tf.python.ops import nn
